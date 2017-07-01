fcf934ab10ad157d485e54e59494c6be853fba2d
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-01 10:42:03 -0700

    change layer 2 & 4. Add scroll magic

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ScrollMagic from 'scrollmagic';
 
-const Content = () => {
-  return (
-    <div className="content">
+class Content extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.scrollAnimation();
+  }
+
+  scrollAnimation() {
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+      duration: 100,
+      offset: 50
+    })
+    .setClassToggle('#scrollTest', 'hidden')
+    .addTo(controller);
+  }
+
+  render() {
+    return (
+      <div className="content">
+      <div id="scrollTest" className="hidden">Hello World</div>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Content;
