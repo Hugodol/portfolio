6e36578a16f8916e7cc1329f5e261e6a0374fb59
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-08 11:28:22 -0700

    add styling and logos to projects cards

--- src/components/ProjectEntry.jsx
+++ src/components/ProjectEntry.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ProjectEntry = (props) => {
-  return (
-    <div className="project">
-      <div className="projectName">
-        {props.name}
+class ProjectEntry extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.changeBackground();
+  }
+
+  changeBackground() {
+    const project = document.getElementById(this.props.name);
+    project.style.backgroundImage = `url(${this.props.background})`;
+  }
+
+  render() {
+    return (
+      <div className="project" id={this.props.name}>
+        <div className="projectName">
+          {this.props.name}
+        </div>
+        <img className="projectLogo" src={this.props.logo} />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProjectEntry;
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -3,6 +3,9 @@ import '../../styles/projects.scss';
 import dividerLarge from '../../assets/images/dividers/divider_projects_large.png';
 import dividerMedium from '../../assets/images/dividers/divider_projects_medium.png';
 import dividerSmall from '../../assets/images/dividers/divider_projects_small.png';
+import cloudMix from '../../assets/images/projects/cloudmix_logo.png';
+import foodtrac from '../../assets/images/projects/foodtrac_logo.png';
+import robodget from '../../assets/images/projects/robodget_logo.png';
 import ProjectEntry from './ProjectEntry';
 import ProjectsButtons from './buttons/ProjectsButtons';
 
@@ -11,9 +14,9 @@ class ProjectsList extends Component {
     super();
     this.state = {
       projects: [
-        {name: 'cloudMix'},
-        {name: 'foodtrac'},
-        {name: 'roBodoget 2.0'},
+        {name: 'cloudMix', logo: cloudMix, background: 'http://wallpapercave.com/wp/OLrPvT4.jpg'},
+        {name: 'roBodoget 2.0', logo: robodget, background: 'https://img06.deviantart.net/6326/i/2010/032/9/b/deep_space_nebula_by_hameed.jpg'},
+        {name: 'foodtrac', logo: foodtrac, background: 'https://images8.alphacoders.com/768/768202.jpg'},
       ],
     };
   }
@@ -27,7 +30,7 @@ class ProjectsList extends Component {
         <h3 className="title_right title">my apps</h3>
         <div className="projects_list">
           {this.state.projects.map(project =>
-            <ProjectEntry name={project.name} />
+            <ProjectEntry name={project.name} logo={project.logo} background={project.background} />
           )}
         </div>
         <ProjectsButtons />
