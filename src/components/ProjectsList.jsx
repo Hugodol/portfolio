afe70b25b49deaf0445e5652987fab87ca33cc83
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-08 16:47:56 -0700

    modularize porjects info

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -3,9 +3,7 @@ import '../../styles/projects.scss';
 import dividerLarge from '../../assets/images/dividers/divider_projects_large.png';
 import dividerMedium from '../../assets/images/dividers/divider_projects_medium.png';
 import dividerSmall from '../../assets/images/dividers/divider_projects_small.png';
-import cloudMix from '../../assets/images/projects/cloudmix_logo.png';
-import foodtrac from '../../assets/images/projects/foodtrac_logo.png';
-import robodget from '../../assets/images/projects/robodget_logo.png';
+import myProjects from '../my_projects.js';
 import ProjectEntry from './ProjectEntry';
 import ProjectsButtons from './buttons/ProjectsButtons';
 
@@ -13,26 +11,7 @@ class ProjectsList extends Component {
   constructor() {
     super();
     this.state = {
-      projects: [
-        {
-          name: 'cloudMix',
-          logo: cloudMix,
-          background: 'http://wallpapercave.com/wp/OLrPvT4.jpg',
-          url: 'https://cloud-mix.herokuapp.com/',
-        },
-        {
-          name: 'roBodoget 2.0',
-          logo: robodget,
-          background: 'https://img06.deviantart.net/6326/i/2010/032/9/b/deep_space_nebula_by_hameed.jpg',
-          url: 'https://robodget.herokuapp.com/',
-        },
-        {
-          name: 'foodtrac',
-          logo: foodtrac,
-          background: 'https://images8.alphacoders.com/768/768202.jpg',
-          url: 'https://foodtrac.me',
-        },
-      ],
+      projects: myProjects,
     };
   }
 
---
+++ src/my_projects.js
@@ -0,0 +1,24 @@
+import cloudMix from '../assets/images/projects/cloudmix_logo.png';
+import foodtrac from '../assets/images/projects/foodtrac_logo.png';
+import robodget from '../assets/images/projects/robodget_logo.png';
+
+export default [
+  {
+    name: 'cloudMix',
+    logo: cloudMix,
+    background: 'http://wallpapercave.com/wp/OLrPvT4.jpg',
+    url: 'https://cloud-mix.herokuapp.com/',
+  },
+  {
+    name: 'roBodget 2.0',
+    logo: robodget,
+    background: 'https://img06.deviantart.net/6326/i/2010/032/9/b/deep_space_nebula_by_hameed.jpg',
+    url: 'https://robodget.herokuapp.com/',
+  },
+  {
+    name: 'foodtrac',
+    logo: foodtrac,
+    background: 'https://images8.alphacoders.com/768/768202.jpg',
+    url: 'https://foodtrac.me',
+  },
+];
