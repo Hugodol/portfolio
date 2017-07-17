ef5946f5116fc2fc2acdc0c97f3ead11717d9f8d
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-17 11:55:13 -0700

    Add extra resize step to skills divider

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import '../../styles/skills.scss';
 import dividerLarge from '../../assets/images/dividers/divider_skills_large.png';
 import dividerMedium from '../../assets/images/dividers/divider_skills_medium.png';
-// import dividerSmall from '../../assets/images/dividers/divider_skills_small.png';
+import dividerSmall from '../../assets/images/dividers/divider_skills_small.png';
 import dividerXSmall from '../../assets/images/dividers/divider_skills_x-small.png';
 import mySkills from '../my_skills';
 import SkillEntry from './SkillEntry';
@@ -22,6 +22,7 @@ class Skills extends Component {
       <div className="skills">
         <img src={dividerLarge} className="divider_left divider_large" alt="" />
         <img src={dividerMedium} className="divider_left divider_medium" alt="" />
+        <img src={dividerSmall} className="divider_left divider_small" alt="" />
         <img src={dividerXSmall} className="divider_left divider_x-small" alt="" />
         <h1 className="title_left title">skills</h1>
         <div className="mySkills">
