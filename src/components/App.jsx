ab8dd8e6b7b838e271eb2b34e598c0221f7bc86e
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-08-05 13:18:51 -0700

    Fix CORS issue with audio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,9 +22,9 @@ class App extends Component {
   initMusic() {
     const song = new Howl({
       src: ['https://s3-us-west-1.amazonaws.com/hugodolportfolio/audio/backgroundSong.mp3'],
-      // autoplay: true,
       loop: true,
       volume: 0.5,
+      html5: true,
     });
     this.setState({ song });
     song.once('load', () => song.play());
