440647b551cbbbccb5cdccd477204fa0623fff71
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-08-05 14:59:18 -0700

    Clean up code

--- data/about.js
+++ data/about.js
@@ -1,5 +1,5 @@
 export default {
   paragraph1: 'Hi, my name is Hugo Dolemieux, a Los Angeles based, design driven, software engineer with a preference for Front-End developing.',
   paragraph2: 'I like to call myself a "professional casual gamer"! I love to get lost in other worlds and epic stories.',
-  paragraph3: 'Navigate to "MyApps" to check out some of my recent projects or go take a look at my skills! Whatever you do, enjoy the rest of your day!',
+  paragraph3: 'Navigate to "my apps" to check out some of my recent projects or go take a look at my skills! Whatever you do, enjoy the rest of your day!',
 };
